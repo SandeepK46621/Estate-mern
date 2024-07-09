688a274d7924c70a7b11674ad912ea492d476fe5
SandeepK46621 <sandeep9511868428@gmail.com>
2024-07-10 00:33:54 +0530

    created and auth and password hashing

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup= async (req,res)=>{
+    const {username,email,password}= req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10);
+    const user = new User({username,email,password:hashedPassword});
+    try{
+        await user.save();
+    res.status(201).json({message:"User created successfully!"});
+    }catch(err){
+        res.status(500).json(err);
+    }
+    
+}
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },email:{
-        type:String,
-        required:true,
-        unique:true
-    },password:{
-        type:String,
-        required:true
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
     }
-}.timestamps);
+}, { timestamps: true });
 
-const User = mongoose.model("User",userSchema);
+
+
+const User = mongoose.model("User", userSchema);
+
+
 export default User;
