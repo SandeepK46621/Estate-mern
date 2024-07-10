56e88585ef604c6427b6f7ce938ba932bc6823f4
SandeepK46621 <sandeep9511868428@gmail.com>
2024-07-11 00:47:13 +0530

    created signin route and used jwt and cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
-
+import { errorHandler } from '../utils/error.js';
+import Jwt from 'jsonwebtoken';
 export const signup= async (req,res,next)=>{
     const {username,email,password}= req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -13,4 +13,24 @@ export const signup= async (req,res,next)=>{
         next(err);
     }
     
+}
+
+export const signin= async (req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"User not found"));
+        }
+        const isValid = bcryptjs.compareSync(password,validUser.password);
+        if(!isValid){
+            return next(errorHandler(401,"Invalid password"));
+        }
+        const token = Jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+
+        res.cookie('access_token',token).status(200).json(validUser);
+
+    }catch(err){
+        next(errorHandler(err))
+    }
 }
